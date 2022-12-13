8c1639810357cd220321e763da5777049d8b47a6
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-13 01:13:58 +0100

    Auswertung in eigene Funktion verschoben.

---
+++ beispiele/roulette/05-ergebnisfunktion/roulette5.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	playerNumber := GetPlayerNumber()
+	wheelNumber := SpinWheel()
+	PrintResult(playerNumber, wheelNumber)
+}
+
+func SpinWheel() int {
+	return 15
+}
+
+func NumberAccepted(number int) bool {
+	return number >= 0 && number <= 36
+}
+
+func ChooseNumber() int {
+	fmt.Print("Bitte wähle eine Zahl: ")
+	var number int
+	fmt.Scanln(&number)
+	return number
+}
+
+func GetPlayerNumber() int {
+	for {
+		number := ChooseNumber()
+		if NumberAccepted(number) {
+			return number
+		}
+		fmt.Println("Das war keine gültige Zahl für's Roulette.")
+		fmt.Println("Bitte probiere es noch einmal!")
+	}
+}
+
+func PrintResult(playerNumber, wheelNumber int) {
+	fmt.Printf("Du hast %d gesetzt, gedreht wurde %d.\n",
+		playerNumber, wheelNumber)
+	if playerNumber == wheelNumber {
+		fmt.Println("Herzlichen Glückwunsch, du hast gewonnen!")
+	} else {
+		fmt.Println("Du hast leider verloren.")
+	}
+}
